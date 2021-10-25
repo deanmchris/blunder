7cf2edac0d0459cc2ca5a8424ce1ce581b232626
Christian Dean <algerbrex@gmail.com>
2021-10-25 14:59:47 -0500

    Refractor SEE function code to be cleaner, based on Stockfish 7's implementation

--- engine/see.go
+++ engine/see.go
@@ -1,5 +1,7 @@
 package engine
 
+import "fmt"
+
 // see.go is a simple implementation of a static exchange evaluator.
 
 var PieceValues [7]int16 = [7]int16{
@@ -8,78 +10,62 @@ var PieceValues [7]int16 = [7]int16{
 	300,
 	500,
 	900,
-	5000,
+	0,
 	0,
 }
 
 // Peform a static exchange evaluation on target square of the move given,
 // and return a score of the move from the perspective of the side to move.
+// Credit to the Stockfish 7 team for the structure of the algorithm below:
+//
+// https://github.com/official-stockfish/Stockfish/blob/dd9cf305816c84c2acfa11cae09a31c4d77cc5a5/src/position.cpp
+//
 func (pos *Position) See(move Move) int16 {
-	if move.MoveType() == Promotion {
-		// Let promotions be evaluated dynamically.
-		return 0
-	}
-
 	fromSq := move.FromSq()
 	toSq := move.ToSq()
-	attackerSq := fromSq
-
-	sideToMove := pos.SideToMove
-	occupiedBB := pos.SideBB[sideToMove] | pos.SideBB[sideToMove^1]
-
 	scores := [32]int16{}
-	attackers := Bitboard(0)
-	seen := Bitboard(0)
+	sideToMove := pos.Squares[fromSq].Color
 
 	scores[0] = PieceValues[pos.Squares[toSq].Type]
-	attackerType := pos.Squares[attackerSq].Type
-	attackerValue := PieceValues[pos.Squares[attackerSq].Type]
+	occupiedBB := pos.SideBB[White] | pos.SideBB[Black]
+	occupiedBB &= ^SquareBB[fromSq]
+
+	if move.MoveType() == Castle {
+		return 0
+	}
 
 	if move.Flag() == AttackEP {
 		capSq := uint8(int8(toSq) - pawnPush(sideToMove))
 		occupiedBB &= ^SquareBB[capSq]
-		scores[0] = PieceValues[Pawn]
+		scores[0] = PieceValues[pos.Squares[capSq].Type]
 	}
 
-	seen |= SquareBB[attackerSq]
+	attackers := allAttackers(pos, toSq, occupiedBB) & occupiedBB
 	sideToMove ^= 1
-	attackers = attackersOfSquare(pos, sideToMove, toSq, occupiedBB, seen)
+	sideToMoveAttackers := attackers & pos.SideBB[sideToMove]
 
-	if attackers == 0 {
+	if sideToMoveAttackers == 0 {
 		return scores[0]
 	}
 
+	captured := pos.Squares[fromSq].Type
 	scoresIndex := 1
+
 	for {
-		occupiedBB &= ^SquareBB[attackerSq]
-		scores[scoresIndex] = attackerValue - scores[scoresIndex-1]
+		scores[scoresIndex] = -scores[scoresIndex-1] + PieceValues[captured]
+		captured = getLeastValuableAttacker(pos, &attackers, sideToMoveAttackers, toSq)
 
-		attackerSq = getLeastValuableAttackerSq(pos, attackers)
-		attackerType = pos.Squares[attackerSq].Type
-		attackerValue = PieceValues[pos.Squares[attackerSq].Type]
-
-		if attackerValue == PieceValues[Pawn] && isPromoting(sideToMove, toSq) {
-			// Pawn promotions aren't handled in the capture sequence either,
-			// so let them be handled dynamically.
-			return 0
-		}
-
-		seen |= SquareBB[attackerSq]
 		sideToMove ^= 1
-		attackers = attackersOfSquare(pos, sideToMove, toSq, occupiedBB, seen)
+		sideToMoveAttackers = attackers & pos.SideBB[sideToMove]
 
-		if attackers == 0 {
-			break
-		}
-
-		if attackerType == King {
-			scores[scoresIndex] = 0
-			scoresIndex--
+		if sideToMoveAttackers == 0 || captured == King {
 			break
 		}
 
 		scoresIndex++
 	}
+
+	fmt.Println(scores, scoresIndex)
 
 	for ; scoresIndex > 0; scoresIndex-- {
 		scores[scoresIndex-1] = min16(-scores[scoresIndex], scores[scoresIndex-1])
@@ -88,13 +74,34 @@ func (pos *Position) See(move Move) int16 {
 	return scores[0]
 }
 
-func attackersOfSquare(pos *Position, enemyColor, sq uint8, occupiedBB, seen Bitboard) (attackers Bitboard) {
-	enemyBishops := pos.PieceBB[enemyColor][Bishop]
-	enemyRooks := pos.PieceBB[enemyColor][Rook]
-	enemyQueens := pos.PieceBB[enemyColor][Queen]
-	enemyKnights := pos.PieceBB[enemyColor][Knight]
-	enemyKing := pos.PieceBB[enemyColor][King]
-	enemyPawns := pos.PieceBB[enemyColor][Pawn]
+// Calculate a bitboard with all of the attackers for each side of a certian square,
+// including x-ray attacks.
+func allAttackers(pos *Position, sq uint8, occupiedBB Bitboard) (attackers Bitboard) {
+	attackers |= attackersForSide(pos, White, sq, occupiedBB)
+	attackers |= attackersForSide(pos, Black, sq, occupiedBB)
+
+	for {
+		occupiedBB &= ^attackers
+		newAttackers := attackersForSide(pos, White, sq, occupiedBB) & ^attackers
+		newAttackers |= attackersForSide(pos, Black, sq, occupiedBB) & ^attackers
+
+		if newAttackers == 0 {
+			break
+		}
+
+		attackers |= newAttackers
+	}
+
+	return attackers
+}
+
+func attackersForSide(pos *Position, attackerColor, sq uint8, occupiedBB Bitboard) (attackers Bitboard) {
+	enemyBishops := pos.PieceBB[attackerColor][Bishop]
+	enemyRooks := pos.PieceBB[attackerColor][Rook]
+	enemyQueens := pos.PieceBB[attackerColor][Queen]
+	enemyKnights := pos.PieceBB[attackerColor][Knight]
+	enemyKing := pos.PieceBB[attackerColor][King]
+	enemyPawns := pos.PieceBB[attackerColor][Pawn]
 
 	intercardinalRays := genBishopMoves(sq, occupiedBB)
 	cardinalRaysRays := genRookMoves(sq, occupiedBB)
@@ -103,26 +110,42 @@ func attackersOfSquare(pos *Position, enemyColor, sq uint8, occupiedBB, seen Bit
 	attackers |= cardinalRaysRays & (enemyRooks | enemyQueens)
 	attackers |= KnightMoves[sq] & enemyKnights
 	attackers |= KingMoves[sq] & enemyKing
-	attackers |= PawnAttacks[enemyColor^1][sq] & enemyPawns
-	return attackers & ^seen
+	attackers |= PawnAttacks[attackerColor^1][sq] & enemyPawns
+	return attackers
 }
 
 // Get the last valuable attacker from a bitboard of attackers.
-func getLeastValuableAttackerSq(pos *Position, attackers Bitboard) uint8 {
+func getLeastValuableAttacker(pos *Position, allAttackers *Bitboard, sideToMoveAttackers Bitboard, toSq uint8) uint8 {
 	lowestValue := Inf
-	attackerSq := uint8(0)
+	pieceType := NoType
+	pieceSq := uint8(NoSq)
 
-	for attackers != 0 {
-		sq := attackers.PopBit()
-		value := PieceValues[pos.Squares[sq].Type]
+	for sideToMoveAttackers != 0 {
+		sq := sideToMoveAttackers.PopBit()
+		currPieceType := pos.Squares[sq].Type
+		value := PieceValues[currPieceType]
 
 		if value < lowestValue {
+			if currPieceType == Bishop || currPieceType == Rook || currPieceType == Queen {
+				lineBetween := Between[sq][toSq] & ^SquareBB[sq]
+				if (lineBetween & *allAttackers) != 0 {
+					// Even if we've found the least valuable attacker, we need to make sure it isn't
+					// blocked by another piece it's xraying through. If it is, we have to choose
+					// another piece instead.
+					continue
+				}
+			}
+
 			lowestValue = value
-			attackerSq = sq
+			pieceType = currPieceType
+			pieceSq = sq
 		}
 	}
 
-	return attackerSq
+	// Remove the least valuable attacker from the attacker bitboard.
+	*allAttackers &= ^SquareBB[pieceSq]
+
+	return pieceType
 }
 
 // Get the minimum between two numbers.
