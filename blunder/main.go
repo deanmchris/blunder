56d6343dba19f03eb2edf58ed54a2d7dacf7a2f3
Christian Dean <algerbrex@gmail.com>
2021-11-18 01:40:23 -0600

    WIP: update evaluation.go with newly tuned weights

--- blunder/main.go
+++ blunder/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "blunder/tuner"
+import "blunder/engine"
 
 func main() {
-	tuner.RunTuner(true)
-	// var inter engine.UCIInterface
-	// inter.UCILoop()
+	// tuner.RunTuner(true)
+	var inter engine.UCIInterface
+	inter.UCILoop()
 	/*var pos engine.Position
 	pos.LoadFEN("r1bqkb1r/pppn1ppp/3p1n2/8/3NP3/2N5/PPP1QP1P/R1B1KBR1 b Qkq - 2 8")
 	fmt.Println(engine.EvaluatePos(&pos))*/
